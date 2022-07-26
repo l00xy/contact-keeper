ad9aaf3ba8b94c6274fccc0592083a203d17fefb
l00xy <ltarasic@gmail.com>
2022-07-26 04:52:11 +0200

    Contacts & ContactItem Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Home from './components/layout/pages/Home';
-import About from './components/layout/pages/About';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 import ContactState from './context/contact/ContactState';
 import './App.css';
